424fbce2111c5181fdc598d5853249010bd38ae1
Nesma <nesmamohamed94@gmail.com>
2018-12-02 12:23:56 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+sass.compiler = require('node-sass');
+
+const browserSync = require('browser-sync').create();
+
+// Copy CSS files task
+gulp.task('copy:css', () => {
+  return gulp
+    .src('node_modules/normalize.css/normalize.css')
+    .pipe(gulp.dest('public/css'));
+});
+
+// Compile Sass task
+gulp.task('compile:sass', () => {
+  const plugins = [autoprefixer({ grid: true })];
+  return gulp
+    .src('public/scss/main.scss')
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('public/css'));
+});
+
+// Serve task
+gulp.task('serve', ['compile:sass'], function() {
+  browserSync.init({
+    watch: true,
+    server: './',
+    browser: 'google chrome',
+    watchOptions: {
+      ignored: '.gitignore'
+    }
+  });
+
+  gulp
+    .watch('public/scss/**/*.scss', ['compile:sass'])
+    .on('change', browserSync.reload);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+// Default task
+gulp.task('default', ['copy:css']);
+console.log(require('node-sass').info);
